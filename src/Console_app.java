0d413750b1730a599f67f2b890978105c2bc9a1d
Mykola Zhukovskyi <zhukovskyi.nikolay@gmail.com>
2020-10-23 14:51:05 +0300

    finish console_app

--- src/Console_app.java
+++ src/Console_app.java
@@ -1,4 +1,5 @@
 import HW_console.One;
+import HW_console.Three;
 import HW_console.Two;
 
 import java.io.IOException;
@@ -20,5 +21,13 @@ public class Console_app {
 
         Two br_two = new Two();
         br_two.two_app();
+
+        /**
+         * three
+         */
+        System.out.println("Дзвінки!");
+
+        Three br_three = new Three();
+        br_three.three_app();
     }
 }
--- src/HW_console/Three.java
+++ src/HW_console/Three.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Three {
-    public static void main(String[] args) throws IOException {
+    public static void three_app() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Call time (UA): ");
         int c1 = Integer.parseInt(br.readLine());
@@ -13,6 +13,7 @@ public class Three {
         int c2 = Integer.parseInt(br.readLine());
         System.out.print("Call time (USA): ");
         int c3 = Integer.parseInt(br.readLine());
+           int c_all = c1 + c2 + c3;
 
         System.out.print("Call cost UA: ");
         double t1 = Double.parseDouble(br.readLine());
@@ -20,8 +21,9 @@ public class Three {
         double t2 = Double.parseDouble(br.readLine());
         System.out.print("Call cost USA: ");
         double t3 = Double.parseDouble(br.readLine());
-
-        System.out.println("Spent GRN " + c1 * t1 + "\n" + "Spent RUB " + c2 * t2 + "\n" + "Spent USA " + c3 * t3);
+                double t_all = t1 + t2 + t3;
+        System.out.println("Spent GRN = " + c1 * t1 + "\n" + "Spent RUB = " + c2 * t2 + "\n" + "Spent USA = " + c3 * t3 + "\n" +
+                            "All call time = " + c_all + "\n" + "All call cost = " + t_all + "\n" + "Spent all = " + c_all * t_all);
 
     }
 }
